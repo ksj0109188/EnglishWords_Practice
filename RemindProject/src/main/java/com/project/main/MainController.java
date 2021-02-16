a72795ed522d4d8b50f6b87bffec0567e17d328d
kim <ksj0109188>
2021-02-16 22:04:24 +0900

    프로젝트 생성

---
+++ RemindProject/src/main/java/com/project/main/MainController.java
@@ -0,0 +1,22 @@
+package com.project.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("mainController")
+@RequestMapping(value="/main")
+public class MainController {
+
+    @RequestMapping(value="/main.do", method ={RequestMethod.POST,RequestMethod.GET})
+    public ModelAndView main(HttpServletResponse response, HttpServletRequest request) throws Exception{
+        String viewName = (String) request.getAttribute("viewNAme");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+}
