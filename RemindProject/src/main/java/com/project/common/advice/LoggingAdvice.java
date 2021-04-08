fcc38c5c6b9a6453f2ad0593f91340ca756c9c82
kim <tjdwns6481@naver.com>
2021-04-08 21:15:58 +0900

    1. Logging Advice(AOP)적용

---
+++ RemindProject/src/main/java/com/project/common/advice/LoggingAdvice.java
@@ -0,0 +1,92 @@
+package com.project.common.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAdvice.class);
+
+//    @Before("execution(* com.project.*.service.*.*(..)) or " + "execution(* com.project.*.dao.*.*(..))")
+//    public void beforeLogging(JoinPoint jp) {
+//        logger.info("-------------------------------------");
+//        logger.info("------------beforeLogging------------");
+//
+//        logger.info("1:" + Arrays.toString(jp.getArgs()));
+//
+//        logger.info("2:" + jp.getKind());
+//
+//        logger.info("3:" + jp.getSignature().getName());
+//
+//        logger.info("4:" + jp.getTarget().toString());
+//
+//        logger.info("5:" + jp.getThis().toString());
+//    }
+
+//    @After("execution(* com.project.*.service.*.*(..)) or " + "execution(* com.project.*.dao.*.*(..))")
+//    public void afterLogging(JoinPoint jp) {
+//        logger.info("-------------------------------------");
+//        logger.info("-------------afterLogging------------");
+//
+//        logger.info("1:" + Arrays.toString(jp.getArgs()));
+//
+//        logger.info("2:" + jp.getKind());
+//
+//        logger.info("3:" + jp.getSignature().getName());
+//
+//        logger.info("4:" + jp.getTarget().toString());
+//
+//        logger.info("5:" + jp.getThis().toString());
+//
+//    }
+
+    @Around("execution(* com.project.*.service.*.*(..)) or " + "execution(* com.project.*.dao.*.*(..))")
+    public Object aroundLogging(ProceedingJoinPoint pjp) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        logger.info("-------------------------------------");
+        logger.info("------------beforeLogging------------");
+
+        logger.info("1:" + Arrays.toString(pjp.getArgs()));
+
+        logger.info("2:" + pjp.getKind());
+
+        logger.info("3:" + pjp.getSignature().getName());
+
+        logger.info("4:" + pjp.getTarget().toString());
+
+        logger.info("5:" + pjp.getThis().toString());
+
+        Object result = pjp.proceed();
+
+        logger.info("-------------------------------------");
+        logger.info("-------------afterLogging------------");
+
+        logger.info("1:" + Arrays.toString(pjp.getArgs()));
+
+        logger.info("2:" + pjp.getKind());
+
+        logger.info("3:" + pjp.getSignature().getName());
+
+        logger.info("4:" + pjp.getTarget().toString());
+
+        logger.info("5:" + pjp.getThis().toString());
+
+        long endTime = System.currentTimeMillis();
+        logger.info("==============================");
+        logger.info(pjp.getSignature().getName() + " : " + (endTime - startTime)+"ms");
+        logger.info("==============================");
+
+        return result;
+    }
+}
